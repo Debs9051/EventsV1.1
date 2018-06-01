ae6c1b0b89beb670c1bf5f5a25660e54a281e3ae
Debs9051 <39730182+Debs9051@users.noreply.github.com>
2018-06-01 18:49:49 +0530

    Initial commit

---
+++ app/src/main/java/events/tcs/com/events/daytwoclass.java
@@ -0,0 +1,21 @@
+package events.tcs.com.events;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import events.tcs.com.events.R;
+
+public class daytwoclass extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dayytwo,container,false);
+    }
+}
+
